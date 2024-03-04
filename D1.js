b53613707650288280ddf1166e57a6dc5d4a0b5c
Fracco87 <fr.ussia@libero.it>
2024-03-04 15:14:13 +0100

    first commit

---
+++ D1.js
@@ -0,0 +1,68 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/*
+  Alcuni tra i principali datatype in JavaScript sono  number(che indica un tipo di variabile di tipo numerico e che dunque accetta valori dello stesso tipo),
+String(che indica un tipo di variabile di tipo alfaNumerico e che prevede la presenza di apici per racchiudere il nome della variabile), null
+(che indica esistenza di variabile con valore 0 o con stringa vuota  ), undifined(che indica l'esistenza di una variabile lasciata addirittura senza valore o stringhe)
+*/
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+let myName = "Francesco";
+console.log(myName);
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+let numberUno = 12;
+let numberDue = 20;
+let somma = numberUno + numberDue;
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+let x = 12;
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+const myName = Francesco;
+myName = Ussia;
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+let numero = 4;
+x = 12;
+let sottrazione = numero - x;
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+let name1 = "john";
+let name2 = "John";
+if (name1 !== name2) {
+  console.log(name1)
+}
+if (name2.toLowerCase() === name1.toLowerCase());
+console.log(name1);
